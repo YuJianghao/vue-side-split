78c5a28c74b93738cf29b843359d6a90c658bc9e
winwin2011 <yjh2011@live.com>
2022-04-20 00:15:13 +0800

    doc: polish demo

--- playground/src/components/Board.vue
+++ playground/src/components/Board.vue
@@ -1,5 +1,5 @@
 <template>
-  <div h-full w-full flex items-center justify-center p4 overflow-auto font-mono text-sm>
+  <div h-full w-full flex items-center justify-center p4 font-mono text-sm>
     <slot />
   </div>
 </template>
