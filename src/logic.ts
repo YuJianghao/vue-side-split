7796dfdbabcf38467e33cf999b7efbe90140e911
winwin2011 <yjh2011@live.com>
2022-04-20 01:05:12 +0800

    chore: lint

--- src/logic.ts
+++ src/logic.ts
@@ -1,6 +1,6 @@
-import { computed, inject, provide, ref, watch } from "vue"
-import type { InjectionKey, Ref } from "vue"
-import { useAxisDrag } from "./composeables"
+import { computed, inject, provide, ref, watch } from 'vue'
+import type { InjectionKey, Ref } from 'vue'
+import { useAxisDrag } from './composeables'
 
 export interface PartConfig {
   min: number
@@ -17,9 +17,9 @@ function createPart(config: Ref<PartConfig[]>) {
   watch(
     config,
     () => {
-      tmp.value = config.value.map((_) => undefined)
+      tmp.value = config.value.map(_ => undefined)
     },
-    { deep: true, immediate: true }
+    { deep: true, immediate: true },
   )
   function setTmp(idx: number, value: number | undefined) {
     tmp.value[idx] = value
@@ -35,9 +35,9 @@ function createPart(config: Ref<PartConfig[]>) {
     return tmp.value[idx]
   }
   function flash() {
-    tmp.value.map((v, idx) => {
+    tmp.value.forEach((v, idx) => {
       const value = v ?? getValue(idx)
-      setValue(idx,value)
+      setValue(idx, value)
     })
   }
   return {
@@ -51,10 +51,10 @@ function createPart(config: Ref<PartConfig[]>) {
   }
 }
 
-export type PartKey = "left" | "right"
+export type PartKey = 'left' | 'right'
 export type Part = ReturnType<typeof createPart>
 export type SideSplitContext = ReturnType<typeof createSideSplitContext>
-export const key: InjectionKey<SideSplitContext> = Symbol("side-split")
+export const key: InjectionKey<SideSplitContext> = Symbol('side-split')
 export const provideContext = (ctx: SideSplitContext) => provide(key, ctx)
 export const useContext = () => inject(key)!
 
@@ -67,38 +67,40 @@ export function createSideSplitContext() {
     return part
   }
 
-  const left = definePart(ref([]), "left")
-  const right = definePart(ref([]), "right")
+  const left = definePart(ref([]), 'left')
+  const right = definePart(ref([]), 'right')
 
   const split = ref<{ idx: number; key: PartKey } | null>(null)
   const gap = ref<number>(10)
-  const axis = ref<"x" | "y">("x")
+  const axis = ref<'x' | 'y'>('x')
   const vertical = ref(false)
   watch(vertical, (v) => {
-    axis.value = v ? "y" : "x"
+    axis.value = v ? 'y' : 'x'
   })
 
   function getSign(key: PartKey) {
     switch (key) {
-      case "left":
+      case 'left':
         return 1
-      case "right":
+      case 'right':
         return -1
     }
   }
 
   const drag = useAxisDrag(axis, {
     onDrag(source, target) {
-      if (!split.value) return
+      if (!split.value)
+        return
       const key = split.value.key
       const idx = split.value.idx
       const part = partMap.get(key)!
       const delta = (target - source) * getSign(key)
       function update(delta: number, idx: number) {
-        if (idx < 0 || idx >= part.config.value.length) return
+        if (idx < 0 || idx >= part.config.value.length)
+          return
         const min = Math.max(
           part.config.value[idx].min,
-          idx ? gap.value : gap.value / 2
+          idx ? gap.value : gap.value / 2,
         )
         const max = part.config.value[idx].max
         const current = part.getValue(idx)
@@ -106,10 +108,12 @@ export function createSideSplitContext() {
         if (next < min) {
           part.setTmp(idx, min)
           update(next - min, idx - 1)
-        } else if (next > max) {
+        }
+        else if (next > max) {
           part.setTmp(idx, max)
           update(next - max, idx - 1)
-        } else {
+        }
+        else {
           part.setTmp(idx, next)
           update(0, idx - 1)
         }
@@ -117,7 +121,8 @@ export function createSideSplitContext() {
       update(delta, idx)
     },
     onDragEnd() {
-      if (!split.value) return
+      if (!split.value)
+        return
       const part = partMap.get(split.value.key)!
       part.flash()
     },
