d5b03568c5bbf986f31cb4d893ae04c4bedf31e7
winwin2011 <yjh2011@live.com>
2022-04-19 15:44:00 +0800

    feat: drag composeables

---
+++ src/lib/composeables.ts
@@ -0,0 +1,54 @@
+import type { Ref } from 'vue'
+import { ref } from 'vue'
+import { useEventListener, useMouse } from '@vueuse/core'
+
+export interface Point{
+  x: number
+  y: number
+}
+
+export function useDrag({ onDrag, onDragEnd }: {
+  onDrag(source: Point, target: Point): void
+  onDragEnd(source: Point, target: Point): void
+}) {
+  const dragging = ref(false)
+  const { x, y } = useMouse()
+  const source = ref<Point>({ x: 0, y: 0 })
+  const target = ref<Point>({ x: 0, y: 0 })
+  useEventListener(window, 'mousemove', () => {
+    target.value = { x: x.value, y: y.value }
+    onDrag(source.value, target.value)
+  })
+  useEventListener(window, 'mouseup', () => {
+    onDragEnd(source.value, target.value)
+    resetDrag()
+  })
+  function resetDrag() {
+    dragging.value = false
+    source.value = { x: 0, y: 0 }
+    target.value = { x: 0, y: 0 }
+  }
+  function startDrag() {
+    dragging.value = true
+    source.value = { x: x.value, y: y.value }
+  }
+  return { startDrag, resetDrag }
+}
+
+export function useAxisDrag(axis: Ref<'x'|'y'>, cbs: {
+  onDrag(source: number, target: number): void
+  onDragEnd(source: number, target: number): void
+}) {
+  const drag = useDrag({
+    onDrag(source, target) {
+      cbs.onDrag(source[axis.value], target[axis.value])
+    },
+    onDragEnd(source, target) {
+      cbs.onDragEnd(source[axis.value], target[axis.value])
+    },
+  })
+  watch(axis, () => {
+    drag.resetDrag()
+  })
+  return drag
+}
