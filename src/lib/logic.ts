3e333bde5fa22c8434ee8f111e58a8496729926a
winwin2011 <yjh2011@live.com>
2022-04-19 15:58:12 +0800

    feat: basic drag event

--- src/lib/logic.ts
+++ src/lib/logic.ts
@@ -1,23 +1,57 @@
 import { ref } from 'vue'
 import type { Ref } from 'vue'
+import { useAxisDrag } from './composeables'
 
-export interface PartConfig{
+export interface PartConfig {
   min: number
   max: number
   init: number
 }
 
-export function createParts(config: Ref<PartConfig[]>, id: Ref<string>) {
+export const partMap = new Map<string, Part>()
+
+function createPart(config: Ref<PartConfig[]>) {
   const raw = ref<number[]>([])
-  const tmp = ref<(number|undefined)[]>([])
+  const tmp = ref<(number | undefined)[]>([])
   const width = computed(() => {
     return tmp.value.map((t, idx) => t ?? raw.value[idx])
   })
-  watch(config, () => {
-    raw.value = config.value.map(c => c.init)
-    tmp.value = config.value.map(_ => undefined)
-  }, { deep: true, immediate: true })
-  return { raw, tmp, config, id, width }
+  watch(
+    config,
+    () => {
+      raw.value = config.value.map(c => c.init)
+      tmp.value = config.value.map(_ => undefined)
+    },
+    { deep: true, immediate: true },
+  )
+  return { raw, tmp, config, width }
 }
 
-export type Parts = ReturnType<typeof createParts>
+export function definePart(config: Ref<PartConfig[]>, key: string) {
+  const part = createPart(config)
+  partMap.set(key, part)
+}
+
+export type Part = ReturnType<typeof createPart>
+
+export const split = ref<{ idx: number; key: string } | null>(null)
+
+const drag = useAxisDrag(ref('x'), {
+  onDrag(source, target) {
+    if (!split.value)
+      return
+    const key = split.value?.key
+    const part = partMap.get(key)!
+    console.log('drag', { source, target })
+  },
+  onDragEnd(source, target) {
+    if (!split.value)
+      return
+    console.log('dragEnd', { source, target })
+  },
+})
+
+export const startDrag = (idx: number, key: string) => {
+  split.value = { idx, key }
+  drag.startDrag()
+}
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,27 +1,29 @@
 <script setup lang="ts">
+/* eslint-disable no-console */
 import { MainPart, SidePart, SideSplit, SplitterPart } from '~/lib/components'
-import { createParts } from '~/lib/logic'
-const parts = createParts(ref([
+import { definePart, partMap, startDrag } from '~/lib/logic'
+definePart(ref([
   { min: 50, max: 150, init: 100 },
   { min: 50, max: 150, init: 100 },
   { min: 50, max: 150, init: 100 },
-]), ref('left'))
+]), 'left')
+const left = partMap.get('left')!
 </script>
 
 <template>
   <SideSplit w-full h-100 border>
-    <SidePart :width="parts.width.value[0]">
+    <SidePart :width="left.width.value[0]">
       side1
     </SidePart>
-    <SplitterPart />
-    <SidePart :width="parts.width.value[1]">
+    <SplitterPart @mousedown="startDrag(0,'left')" />
+    <SidePart :width="left.width.value[1]">
       side2
     </SidePart>
-    <SplitterPart />
-    <SidePart :width="parts.width.value[2]">
+    <SplitterPart @mousedown="startDrag(1,'left')" />
+    <SidePart :width="left.width.value[2]">
       side3
     </SidePart>
-    <SplitterPart />
+    <SplitterPart @mousedown="startDrag(2,'left')" />
     <MainPart>
       main
     </MainPart>
